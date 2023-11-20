809dd71917bf02f31fd2484bcbf6b1ab3bbf7c3d
yan monteiro <yanlinhares9@hotmail.com>
2023-11-20 17:33:05 -0300

    first-user-setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address])
+  end
 end

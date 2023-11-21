fa36266a7542bec8ee1f9f8475486cd3fae73e54
luiz fernando sales <lnandofonseca@gmail.com>
2023-11-21 16:14:55 -0300

    new-changes

---
+++ app/controllers/cars_controller.rb
@@ -0,0 +1,51 @@
+class CarsController < ApplicationController
+  before_action :set_car, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @cars = Car.all
+  end
+
+  def show
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    @car.user = current_user
+
+    if @car.save
+      redirect_to cars_path(@car)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @car.update(car_params)
+      redirect_to @car, notice: 'Carro atualizado com sucesso!'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @car.destroy
+    redirect_to cars_path, notice: 'Carro excluído com sucesso!'
+  end
+
+  private
+
+  def set_car
+    @car = Car.find(params[:id])
+  end
+
+  def car_params
+    params.require(:car).permit(:manufacturer, :model, :year, :km, :price, :color)
+  end
+end

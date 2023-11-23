24521c1f3b334627bf618938c895a18060886f5f
Luis Borges <lfelipepeixoto92@gmail.com>
2023-11-23 17:48:19 -0300

    back-end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,10 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:perfil]
 
   def perfil
-
     @user = current_user
+  end
+
+  def sign_out
+    
   end
 end
